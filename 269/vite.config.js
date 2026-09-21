d26b0ee44a3aa51b89d4f247eb54da944dda37e2
uzb754 <xojimurodovjaloliddin6@gmail.com>
2026-09-21 20:22:08 +0500

    Base path to'g'rilandi va build yangilandi

--- 269/vite.config.js
+++ 269/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // <--- Mana bu yerda faqat slash qolishi kerak
+  base: '/', // Bu yerda faqat / turishi shart, maktab-sayt bo'lmasligi kerak!
 })
