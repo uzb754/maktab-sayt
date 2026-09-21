8fd80aad762fd30576cade6cd589d4852f7f2ed8
uzb754 <xojimurodovjaloliddin6@gmail.com>
2026-09-21 21:28:36 +0500

    Build yangilandi va sozlamalar to'g'rilandi

--- 269/vite.config.js
+++ 269/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Bu yerda faqat / turishi shart, maktab-sayt bo'lmasligi kerak!
+  base: '/',
 })
