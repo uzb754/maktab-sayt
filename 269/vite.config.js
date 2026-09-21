1e3d818d68ad47904bdb5a9a9722eb5ebfb567d4
uzb754 <xojimurodovjaloliddin6@gmail.com>
2026-09-21 07:12:09 +0500

    Domen uchun base path tozalandi

--- 269/vite.config.js
+++ 269/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/maktab-sayt/', // <--- MANA SHU QATORNI QO'SHASIZ
+  base: '/', // <--- Mana bu yerda faqat slash qolishi kerak
 })
