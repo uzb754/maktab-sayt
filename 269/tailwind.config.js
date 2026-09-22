0dc3979f9f3a904290e2387b8898530d0ee60c60
uzb754 <xojimurodovjaloliddin6@gmail.com>
2026-09-22 20:12:00 +0500

    Tailwind content yo'li to'g'irlandi

--- 269/tailwind.config.js
+++ 269/tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./269/src/**/*.{js,ts,jsx,tsx}", // Agar kodlar 269 papkasi ichida bo'lsa
   ],
   theme: {
     extend: {},
